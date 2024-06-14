4642ca51f82a4a980e69485065da1b2f747e7653
yleniapregnolato <yleniapregnolato@gmail.com>
2024-06-14 15:17:51 +0200

    add:comics migration

---
+++ database/migrations/2024_06_14_131238_create_comics_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('thumb')->nullable();
+            $table->string('price');
+            $table->string('series')->nullable();
+            $table->date('sale_date');
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comics');
+    }
+};
