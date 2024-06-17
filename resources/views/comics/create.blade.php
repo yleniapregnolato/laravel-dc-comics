4145ba466aed2b7981eee3977ee14f414baf0f24
yleniapregnolato <yleniapregnolato@gmail.com>
2024-06-17 09:35:53 +0200

    fix: creat.blade.php

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -4,7 +4,7 @@
     <h1>Crea un nuovo fumetto</h1>
 
     <form action="{{ route('comics.store') }}" method="POST">
-
+        @csrf
         <div class="mb-3 me-5 ms-5">
             <label for="titolo" class="form-label">Titolo</label>
             <input type="text" class="form-control" id="titolo" name="titolo">
