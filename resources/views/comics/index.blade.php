70ededa6708125eb9dbf50443217ca5d8e612a29
yleniapregnolato <yleniapregnolato@gmail.com>
2024-06-17 15:20:47 +0200

    add: delete in index.php and destroy function in ComicController.php

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -10,8 +10,13 @@
                 <li class="">
                     <img class="w-25" src="{{ $comic->immagine }}" alt="">
                     <h4>{{ $comic->titolo }}</h2>
-                    <a class="btn border border-black mt-3 mb-3" href="{{ route('comics.show', ['comic' => $comic->id]) }}">Dettagli</a>
+                    <a class="btn border border-black mt-3 mb-3" href="{{ route('comics.show', ['comic' => $comic->id]) }}">Dettagli</a><br>
                     <a class="btn border border-black mt-3 mb-3" href="{{ route('comics.edit', ['comic' => $comic->id]) }}">Modifica</a>
+                    <form action="{{ route('comics.destroy', ['comic' => $comic->id])}}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn border border-black mt-3 mb-3">cancella</button>
+                      </form>
                 </li>
             @endforeach
         </ul>
